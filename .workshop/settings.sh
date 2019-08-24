ba4fe94d39547f3a19cc7ac10c4b2150df655e83
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-08-24 21:28:21 +1200

    Update workshop scripts and set spawner password.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,6 +1,7 @@
 WORKSHOP_NAME=lab-getting-started
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-getting-started:ocp-4.2
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
+SPAWNER_PASSWORD=openshift
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=7200
 IDLE_TIMEOUT=300
