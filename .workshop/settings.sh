54f1f94181fd89b00b48c55ec75b6e1c966a12a1
jankleinert <jkleiner@redhat.com>
2019-10-09 11:48:09 -0400

    Update workshop RESOURCE_BUDGET to x-large

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -4,6 +4,6 @@ WORKSHOP_TITLE="Getting Started with OpenShift"
 WORKSHOP_DESCRIPTION="A workshop introducing developers to OpenShift"
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 SPAWNER_PASSWORD=openshift
-RESOURCE_BUDGET=medium
+RESOURCE_BUDGET=x-large
 MAX_SESSION_AGE=7200
 IDLE_TIMEOUT=300
