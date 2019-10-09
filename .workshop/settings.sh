8a72a6f5e9d261c73164de03f185168d5a5d2d8e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-09 16:51:55 +0800

    Add title and description for homeroom picker application.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,7 @@
 WORKSHOP_NAME=lab-getting-started
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-getting-started:ocp-4.2
+WORKSHOP_TITLE="Getting Started with OpenShift"
+WORKSHOP_DESCRIPTION="A workshop introducing developers to OpenShift"
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 SPAWNER_PASSWORD=openshift
 RESOURCE_BUDGET=medium
