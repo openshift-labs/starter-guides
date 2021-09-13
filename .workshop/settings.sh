bf9d281ac21376cb7ea222fcc2edb844059227d5
Natale Vinto <nvinto@redhat.com>
2021-09-13 14:35:46 +0200

    Update images to 4.8

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,8 +1,8 @@
 WORKSHOP_NAME=lab-getting-started
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-getting-started:ocp-4.6
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-getting-started:ocp-4.8
 WORKSHOP_TITLE="Getting Started with OpenShift"
 WORKSHOP_DESCRIPTION="A workshop introducing developers to OpenShift"
-CONSOLE_IMAGE=quay.io/openshift/origin-console:4.6
+CONSOLE_IMAGE=quay.io/openshift/origin-console:4.8
 RESOURCE_BUDGET=x-large
 MAX_SESSION_AGE=7200
 IDLE_TIMEOUT=300
